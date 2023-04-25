230ea86b099f587a3030ba0cdabff36942cb6361
lebakhai13 <lebakhai1312@gmail.com>
2023-04-25 12:00:58 +0700

    fix rate

--- JS/assest/js/main.js
+++ JS/assest/js/main.js
@@ -5,7 +5,7 @@ var countJS = 0;
 var countSuccesJS = 0;
 function gacha() {
     var random = Math.floor(Math.random() * 100);
-    if (random = (1)) {
+    if (random <= 1) {
         boxElement.innerHTML = '<p>có:</p><br><img id="img" src="https://static.vecteezy.com/system/resources/previews/000/421/448/original/weapons-icon-vector-illustration.jpg">';
         countSuccesJS += 1;
         countSuccesElement.textContent = `Success: ${countSuccesJS}`;

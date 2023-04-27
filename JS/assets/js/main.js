5d512a11c08c0ce0e4e7b78b0a6e74160f94acd3
lebakhai13 <lebakhai1312@gmail.com>
2023-04-27 17:21:55 +0700

    fix assets spell: assest --> assest

---
+++ JS/assets/js/main.js
@@ -0,0 +1,27 @@
+var boxElement = document.querySelector('.box');
+var countElement = document.querySelector('.countHTML');
+var countSuccesElement = document.querySelector('.countSuccesHTML');
+var countJS = 0;
+var countSuccesJS = 0;
+function gacha() {
+    var random = Math.floor(Math.random() * 100);
+    if (random <= 1) {
+        boxElement.innerHTML 
+        = '<p>có:</p><br><img id="img" src="https://cdn1.iconfinder.com/data/icons/operating-system-flat-1/30/linux-512.png">';
+        countSuccesJS += 1;
+        countSuccesElement.textContent = `Success: ${countSuccesJS}`;
+    } else {
+        boxElement.textContent = 'Có cái dái';
+        countJS += 1;
+        countElement.textContent = `Fail: ${countJS}`;
+    }
+};
+
+function randomColor() {
+    var color1 = Math.floor(Math.random() * 255);
+    var color2 = Math.floor(Math.random() * 255);
+    var color3 = Math.floor(Math.random() * 255);
+    boxElement.style.color = `rgb(${color1}, ${color2}, ${color3})`;
+};
+
+// setInterval(randomColor, 1000);

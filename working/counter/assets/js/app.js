324e2b3aabadcc794400f914e0bb03039d3b2677
lebakhai13 <lebakhai1312@gmail.com>
2023-05-17 16:32:40 +0700

    Optimize counter: onclick with separate function

--- working/counter/assets/js/app.js
+++ working/counter/assets/js/app.js
@@ -3,6 +3,7 @@ const $$ = document.querySelectorAll.bind(document);
 
 const numberElement = $('.number');
 const btns = Array.from($$('.btn'));
+const control = $('.control')
 
 var counterNumber = 0;
 
@@ -16,22 +17,22 @@ function resetNumber() {
     numberElement.textContent = counterNumber;
 }
 
-btns.forEach((btn, index) => {
+function clickHandler(btn) {
     if (btn.classList.contains('decrease')) {
-        btn.onclick = (e) => {
-            getNumber(-1);
-        }
+        getNumber(-1);
     } 
     
     if (btn.classList.contains('reset')) {
-        btn.onclick = (e) => {
-            resetNumber();
-        }   
+        resetNumber();
     } 
 
     if (btn.classList.contains('increase')) {
-        btn.onclick = (e) => {
-            getNumber(1);
-        }
+        getNumber(1);
+    }
+}
+
+btns.forEach((btn) => {
+    btn.onclick = (e) => {
+        clickHandler(btn);
     }
 })

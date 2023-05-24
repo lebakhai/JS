ae2a66829ef28bc8e0e5286fca304dd37996d22d
lebakhai13 <lebakhai1312@gmail.com>
2023-05-24 13:59:23 +0700

    Add: complete library

--- working/buildUI/assets/js/core.js
+++ working/buildUI/assets/js/core.js
@@ -6,6 +6,30 @@ export default function html([first, ...strings], ...values) {
     .join('');
 };
 
-export function createStore() {
+export function createStore(reducer) {
+    let state = reducer();
+    const roots = new Map();
 
+    function render() {
+        for (const [root, component] of roots) {
+            const output = component();
+            root.innerhHTML = output;
+        };
+    };
+
+    return {
+        attach(component, root) {
+            roots.set(root, component);
+            render();
+        },
+        connect(selector = state => state) {
+            return component => (props, ...args) => {
+                component(Object.assign({}, props, selector(state), ...args));
+            }
+        },
+        dispatch(action, ...args) {
+            state = reducer(state, action, args);
+            render()
+        }
+    };
 };
